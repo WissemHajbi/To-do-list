d6cee04c7a45834f2714b81e366be034865b78f7
wissem hajbi <wissemhajbi2002@gmail.com>
2021-10-22 17:32:23 +0100

    staticfile insert

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -121,10 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILE_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
