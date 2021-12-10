47d78b94e544da98bd1961b20b00ae6d20ca1875
wissem hajbi <wissemhajbi2002@gmail.com>
2021-12-10 21:35:18 +0100

    formating to pep8 staders

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class goal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=200,null=True,blank=True)
+    description = models.TextField(max_length=200, null=True, blank=True)
     done = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title
+
     class Meta:
         ordering = ['done']
-        
